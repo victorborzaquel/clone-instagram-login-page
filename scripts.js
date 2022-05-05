5e5e4ba8f0d09cce125b62382df636b193b93976
victorborzaquel <victorborzaquel@outlook.com>
2022-05-05 18:55:17 -0300

    refactor: JavaScript

--- scripts.js
+++ scripts.js
@@ -3,12 +3,13 @@ const screens = document.querySelectorAll(".screen")
 let index = 1
 
 function cycleScreen() {
-    const isLastScreen = () => index === screens.length - 1
+    const isLastScreen = index === screens.length - 1
 
     screens.forEach(screen => screen.classList.add("hidden"))
     screens[index].classList.remove("hidden")
 
-    index = isLastScreen() ? 0 : ++index
+    if (isLastScreen) index = 0
+    else index++
 }
 
 setInterval(cycleScreen, 3000)
